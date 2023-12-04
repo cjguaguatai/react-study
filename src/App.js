c9926d985d3dcb87d23e9c6603a86868e1edd35f
xuqingzhe <cjguaguatai@163.com>
2023-12-04 17:01:30 +0800

    65

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import axios from 'axios';
 export default class App extends Component {
+  getStudentData = () => {
+    axios.get('http://localhost:5000/students').then(
+      (res) => {},
+      (req) => {},
+    );
+  };
   render() {
-    return (
-      <div>App</div>
-    )
+    return <div>App</div>;
   }
 }
